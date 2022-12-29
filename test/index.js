949556f318130a6e6f45e1558bf2afbaa9c13fb0
Yisen <yisenliu@gmail.com>
2022-12-29 10:03:43 +0800

    Test of Hosting a JSON API on GitHub Pages

--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
 const msg = document.getElementById("msg");
 axios.get("./test.json").then((res) => {
   console.log(res.data);
-  msg.textContent = JSON.stringify(res.data, null, "\t");
+  const { image, name } = res.data;
+  const html = `
+  <figure>
+    <img src="${image}" alt="${name}" />
+    <figcaption>${name}</figcaption>
+  </figure>
+  `;
+  msg.insertAdjacentHTML("afterbegin", html);
 });
